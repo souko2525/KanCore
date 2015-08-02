6e3e68bebd56b59704a9e8f1ca662511e5304284
souko2525 <yusuke.h0808@gmail.com>
2015-08-02 21:39:35 +0900

    add formation

--- Gemfile
+++ Gemfile
@@ -45,3 +45,10 @@ end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+#
+gem 'seed-fu', '~> 2.3'
+
+group :production do
+   gem 'pg', '0.15.1'
+   gem 'rails_12factor', '0.0.2'
+end
--- app/controllers/damage_controller.rb
+++ app/controllers/damage_controller.rb
@@ -12,13 +12,27 @@ private
   def calc_one(enemies)
     damages = []
     enemies.each do |enemy|
-       max_def = (enemy.defence * 1.3 -0.6)
+       max_def = (enemy.defence * 1.3 -0.6).ceil
+       min_def = (enemy.defence * 0.7).ceil
        base = enemy.hp + max_def
+       if base > 150
+         base = 150 + (base - 150) ** 2
+       end
        doko = base.ceil
        t_u = (base / 1.2).ceil
        t_f = (base / 0.6).ceil
        hanko = (base / 0.8).ceil
-       damage = {"doko" => doko, "t_u" => t_u, "t_f" => t_f, "hanko" => hanko}
+       f_doko = (base / 0.8).ceil
+       f_t_u = (base / 1.2 / 0.8).ceil
+       f_t_f = (base / 0.6 / 0.8).ceil
+       f_hanko = (base / 0.8 / 0.8).ceil
+       r_doko = (base / 0.7).ceil
+       r_t_u = (base / 1.2 / 0.7).ceil
+       r_t_f = (base / 0.6 / 0.7).ceil
+       r_hanko = (base / 0.8 / 0.7).ceil
+       damage = {"doko" => doko, "t_u" => t_u, "t_f" => t_f, "hanko" => hanko,
+                 "f_doko" => f_doko, "f_t_u" => f_t_u, "f_t_f" => f_t_f, "f_hanko" => f_hanko,
+                 "r_doko" => r_doko, "r_t_u" => r_t_u, "r_t_f" => r_t_f, "r_hanko" => r_hanko}
        damages.push(damage)
     end
     return damages
